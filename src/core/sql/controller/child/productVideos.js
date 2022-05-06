0a3a82d41a6ecb7bb7085b456270fc79328e7c06
Sambhav Jain <sambhavjain@MacBook-Pro.local>
2022-05-06 18:34:43 +0530

    bugs removed

--- src/core/sql/controller/child/productVideos.js
+++ src/core/sql/controller/child/productVideos.js
@@ -12,7 +12,7 @@ class ProductVideos extends AbstractSQL{
     this.connection = super.connection();
   }
   
-  saveProductVideo(productId, videos) {
+  saveProductVideo(productId, video) {
     return new Promise((resolve, reject) => {
       this.connection.query(QUERY_BUILDER.SAVE_PRODUCT_VIDEO(productId, video), super.getQueryType('INSERT')).then(result => {
         resolve(result);
@@ -55,6 +55,7 @@ const QUERY_BUILDER = {
       [PRODUCT_VIDEOS_FIELDS.NAME] : video?.name || '',
       [PRODUCT_VIDEOS_FIELDS.THUMBNAIL] : video?.thumbnail || '',
       [PRODUCT_VIDEOS_FIELDS.DESCRIPTION] : video?.description || '',
+      [PRODUCT_VIDEOS_FIELDS.SLUG] : video?.slug
     }
     return SqlString.format(`INSERT INTO ${PRODUCT_VIDEOS_TABLE_NAME} SET ?`, data)
   },
