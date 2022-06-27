72cef167d3242cf594c884862868873b34906082
Sambhav Jain <sambhavjain@MacBook-Pro.local>
2022-06-28 01:37:32 +0530

    added razorpay integration

--- src/routes/v1/orders/new.js
+++ src/routes/v1/orders/new.js
@@ -1,5 +1,6 @@
 const ApiError = require("../ApiError");
 const {Product, Orders } = require("../../../core/sql/controller/child");
+const Razorpay = require("razorpay");
 
 const newOrder = {};
 
@@ -11,8 +12,7 @@ const newOrder = {};
 */
 newOrder.validateRequest = async(req, res, next) => {
   const { productId, variantId, quantity, addressId } = req.body;
-  const userId =2;
-  if(!( userId && productId && variantId && quantity && addressId)) next(new ApiError(400, 'E0010002', {}, 'Invalid request! Please check your inputs'));
+  if(!( productId && variantId && quantity && addressId)) next(new ApiError(400, 'E0010002', {}, 'Invalid request! Please check your inputs'));
   next();
 }
 
@@ -25,14 +25,25 @@ newOrder.validateRequest = async(req, res, next) => {
 newOrder.new = async(req, res, next) => {
   try {
     const { productId, variantId, quantity, addressId } = req.body;
+    const userId = req._userId;
+    
     const OrdersObj = new Orders(req._siteId);
     const product = await OrdersObj.variantsByVariantId(variantId);
     if(product){
-        let notes = req.body.notes 
-        let param = {userid:2, variantId, productId, quantity, addressId, notes, status:"pending", deliveryStatus:"processing", priceBeforeTax :product.price, priceAfterTax :product.price, discount : 0, tax : 0}
-        const response = await OrdersObj.newOrder(param)
-        req._response = response;
-        next();
+      var instance = new Razorpay({ key_id: process.env['RAZORPAY:KEY_ID'], key_secret: process.env['RAZORPAY:KEY_SECRET'] })
+
+      const createdOrder = await instance.orders.create({
+        amount: 50000,
+        currency: "INR",
+        receipt: "receipt#1"
+      })
+      
+      let notes = req.body.notes 
+      let param = {userid: userId, variantId, productId, quantity, razorpayOrderId: createdOrder?.['id'], addressId, notes, status:"pending", deliveryStatus:"processing", priceBeforeTax :product.price, priceAfterTax :product.price, discount : 0, tax : 0}
+      const response = await OrdersObj.newOrder(param)
+      createdOrder.hoppedinOrderId = response
+      req._response = createdOrder;
+      next();
     }else{
         return next(new ApiError(400, 'E0010006'));
     }
